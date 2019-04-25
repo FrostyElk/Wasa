c7277adbdc286f32c85fd8d032aed5c45dd8cca4
Arne Sikstrom <arne.sikstrom@gmail.com>
2019-04-25 10:56:41 +0200

    def

--- app/forms.py
+++ app/forms.py
@@ -11,7 +11,7 @@ from wtforms.validators import DataRequired
 class RconAuthForm(FlaskForm):
     ip_address = StringField(u'Server IP Address', validators=[DataRequired()], default='localhost')
     rcon_port = IntegerField(u'RCON Port', validators=[DataRequired()], default=27888)
-    password = StringField(u'RCON Password', validators=[DataRequired()], default=')
+    password = StringField(u'RCON Password', validators=[DataRequired()], default='')
 
     submit = SubmitField(u'Connect')
 
